5cb556e5808d2c8f271995bb2879423abdbbbb79
2Chaos <ace.wonder@qq.com>
2023-05-02 21:02:25 +0800

    test

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import App, { TodoListItem } from "@2chaos/webpack_react_todolist";
+const TodolistPage = () => {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+};
+console.log(App, TodoListItem);
+export default TodolistPage;

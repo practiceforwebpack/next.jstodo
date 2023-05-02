763136c1bf2365b067c9798ff520ca78673d5418
2Chaos <ace.wonder@qq.com>
2023-05-02 22:00:38 +0800

    删掉了；

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ const TodolistPage = () => {
         name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
       ></meta>
-      ;
       <App />
     </div>
   );

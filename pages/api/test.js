9b4238c7972fe73aac20c915157f1d27ab0d3ee7
2chaos <1585767724@qq.com>
2023-05-04 00:10:56 +0800

    修改路径

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-fetch("/api/hello")
+fetch("/hello")
   .then((res) => res.json())
   .then((data) => console.log(data))
   .catch((err) => console.error(err));

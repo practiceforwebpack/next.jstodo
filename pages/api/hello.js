ec1ece66a1092acda2718de58c9b29ca5b4a7224
2chaos <1585767724@qq.com>
2023-05-03 23:37:26 +0800

    1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,7 @@
 export default (req, res) => {
   res.status(200).json({ message: "Hello, Next.js API!" });
 };
+fetch("/api/hello")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
--- pages/api/test.js
+++
@@ -1,4 +0,0 @@
-fetch("/api/hello")
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.error(err));

e5330f83534369dbd4a48a2f5b7218f652380f69
2chaos <1585767724@qq.com>
2023-05-04 17:53:55 +0800

    try

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,7 +4,12 @@ function Home() {
   const [message, setMessage] = useState(null);
 
   async function handleClick() {
-    const response = await fetch("/api/hello");
+    const response = await fetch("/api/hello", {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
     const data = await response.json();
     setMessage(data.message);
   }

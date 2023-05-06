29c150a18e1de40cb932bb7862cc20cc5bcec7ab
2chaos <1585767724@qq.com>
2023-05-06 23:10:01 +0800

    删减筛选内容

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useState } from "react";
 import App, { TodoListItem } from "@2chaos/webpack_react_todolist";
+import * as cheerio from "cheerio";
 const TodolistPage = () => {
+  const [url, setUrl] = useState("");
+  const [state, updateState] = useState("");
+
   return (
     <div>
       <meta
@@ -9,6 +13,38 @@ const TodolistPage = () => {
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
       ></meta>
       <App />
+
+      <input
+        style={{ width: 1293 }}
+        placeholder="请输入网址并按下回车"
+        value={state}
+        onChange={(e) => {
+          updateState(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            setUrl(state);
+          }
+        }}
+        onKeyUp={async (e) => {
+          if (e.key === "Enter") {
+            console.log(url);
+            try {
+              const response = await fetch("/api/fetch-url-input", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ url }),
+              });
+              const data = await response.json();
+              console.log(data);
+            } catch (error) {
+              console.error(error);
+            }
+            updateState("");
+          }
+        }}
+      ></input>
+
       <button
         onClick={() =>
           fetch("/api/hello")
@@ -16,9 +52,10 @@ const TodolistPage = () => {
             .then((e) => console.log(e))
         }
       >
-        XXX
+        test
       </button>
     </div>
   );
 };
+
 export default TodolistPage;

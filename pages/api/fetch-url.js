0f48a2df00b032fcce32c6ece5062f9eb7817d38
2chaos <1585767724@qq.com>
2023-05-17 23:44:25 +0800

    添加请求头部信息

--- pages/api/fetch-url.js
+++ pages/api/fetch-url.js
@@ -6,7 +6,15 @@ import got from "got";
 export default async function handler(req, res) {
   const { url } = req.body;
   try {
-    const response = await got(url);
+    const options = {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        Referer: url,
+      },
+    };
+
+    const response = await got(url, options);
     const $ = cheerio.load(response.body);
     const title = $("title").text();
     const firstImgSrc = $("img").eq(0).attr("src");

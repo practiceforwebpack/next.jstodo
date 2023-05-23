5db82876b31e4e603b98e947c22108d0810aaba8
2chaos <1585767724@qq.com>
2023-05-23 22:12:28 +0800

    完整版提交

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import { useState, useEffect } from "react";
+
+const isValidURL = (str) => {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}" + // domain name
+      "|((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+};
 
 export default function Home() {
   const [cardHTML, setCardHTML] = useState("");
-  const [pageTitle, setPageTitle] = useState("");
+  const [docTitle, setDocTitle] = useState("");
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const url = urlParams.get("url");
     if (!url) {
+      // If no URL is in the query parameter, do not attempt to fetch
       return;
     }
     if (!isValidURL(url)) {
-      setPageTitle("Invalid URL");
       setCardHTML("Invalid URL");
       return;
     }
@@ -21,10 +34,18 @@ export default function Home() {
       try {
         const response = await fetch(`/api/fetch-url?url=${url}`);
         const data = await response.text();
-        const parsedHTML = new DOMParser().parseFromString(data, "text/html");
-        const title = parsedHTML.querySelector("title");
-        setPageTitle(title?.textContent || "Fetch URL Card");
         setCardHTML(data);
+
+        // extract the title from the fetched HTML
+        console.log("data", data);
+        const titleMatch = data.match(/<h2>(.*?)<\/h2>/i);
+        if (titleMatch) {
+          const newTitle = titleMatch[1]; // get the title from HTML
+          document.title = newTitle; // set the browser tab title
+          setDocTitle(newTitle); // set the document title
+        } else {
+          console.log("Title not found!");
+        }
       } catch (error) {
         console.error(error);
         setCardHTML("An error occurred");
@@ -36,31 +57,16 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>{pageTitle || "Fetch URL Card"}</title>
+        <title>{docTitle || "Fetch URL Card"}</title>
         <meta name="description" content="Fetch URL Card" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        {cardHTML ? (
-          <div dangerouslySetInnerHTML={{ __html: cardHTML }} />
-        ) : (
-          <p>No URL was provided in the query parameter.</p>
-        )}
-      </main>
-
-      <style jsx>{`
-        // your CSS styles
-      `}</style>
+      {cardHTML ? (
+        <main dangerouslySetInnerHTML={{ __html: cardHTML }} />
+      ) : (
+        <main>No URL was provided in the query parameter.</main>
+      )}
     </div>
   );
 }
-
-const isValidURL = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-};

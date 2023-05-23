c6a621ae73b5cd723b69a796a2b9d4ea614ec911
2chaos <1585767724@qq.com>
2023-05-23 19:58:32 +0800

    基础版本

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import { useState, useEffect } from "react";
-
+import { useState } from "react";
+//,
 export default function Home() {
   const [url, setUrl] = useState("");
   const [cardHTML, setCardHTML] = useState("");
-
-  const fetchData = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const response = await fetch(`/api/fetch-url?url=${url}`);
+      const response = await fetch("/api/fetch-url", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url }),
+      });
       const data = await response.text();
       setCardHTML(data);
     } catch (error) {
@@ -15,40 +19,32 @@ export default function Home() {
     }
   };
 
-  useEffect(() => {
-    if (url) {
-      fetchData();
-    }
-  }, [url]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetchData();
-  };
-
   return (
-    <div className="flex flex-col items-center">
-      <form className="mt-4 flex flex-col" onSubmit={handleSubmit}>
+    <div class="flex flex-col items-center">
+      {" "}
+      <form class="mt-4 flex flex-col" onSubmit={handleSubmit}>
+        {" "}
         <input
-          className="h-10 px-2 border rounded-lg"
+          class="h-10 px-2 border rounded-lg"
           type="text"
-          placeholder="Enter URL and press Enter"
+          placeholder="输入url按回车键确认"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
-        />
+        />{" "}
         <button
-          className="h-10 bg-blue-500 text-white rounded-lg px-4 ml-2"
+          class="h-10 bg-blue-500 text-white rounded-lg px-4 ml-2"
           type="submit"
         >
-          Fetch Data
-        </button>
-      </form>
+          {" "}
+          Fetch Data{" "}
+        </button>{" "}
+      </form>{" "}
       {cardHTML && (
         <div
-          className="w-320 h-160 flex items-center bg-white rounded-lg shadow-md overflow-hidden justify-center"
+          class="w-320 h-160 flex items-center bg-white rounded-lg shadow-md overflow-hidden justify-center"
           dangerouslySetInnerHTML={{ __html: cardHTML }}
         ></div>
-      )}
+      )}{" "}
     </div>
   );
 }

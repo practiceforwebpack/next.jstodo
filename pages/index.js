2b91b3338225877393425c872f224cb625d9f4c0
2chaos <1585767724@qq.com>
2023-05-29 13:47:46 +0800

    再次修改阴影

--- pages/index.js
+++ pages/index.js
@@ -172,7 +172,9 @@ export default function Home() {
           background-color: ${loading ? "#F2F2F2" : "#fff"};
           border-radius: 10px;
           border-radius: 5px;
-          box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+          box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+            rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+            rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
           overflow: hidden;
         }
 

39b154c59e11d089b2d1ec9a2ca97f818ce8c7d1
2chaos <1585767724@qq.com>
2023-06-01 22:08:24 +0800

    网页调试

--- pages/api/fetch-url.js
+++ pages/api/fetch-url.js
@@ -1,28 +1,26 @@
-import { isValidURL, decodeURI, fetchHTML, extractData } from "./helpers.js";
+const cheerio = require("cheerio");
+const axios = require("axios");
 
-export default async function handler(req, res) {
-  let url = req.query.url || req.body.url;
-  if (!url && req.method === "GET") {
-    const urlParams = new URLSearchParams(req.url.slice(req.url.indexOf("?")));
-    url = urlParams.get("url");
-  }
-  if (!url) {
-    return res.status(400).json({ message: "URL is required" });
-  }
-
-  url = decodeURI(url);
-  if (!isValidURL(url)) {
-    return res.status(400).json({ message: "Invalid URL" });
-  }
+// 定义路由处理函数
+async function handler(req, res) {
+  const { url } = req.query; // 获取参数中的 url
 
   try {
-    const html = await fetchHTML(url);
-    const data = extractData(html);
+    // 发送 GET 请求获取网站 HTML 内容
+    const response = await axios.get(url);
 
-    const result = { ...data, url };
-    res.status(200).json(result);
+    // 使用 cheerio 将 HTML 文本转换成 DOM 对象
+    const $ = cheerio.load(response.data);
+
+    // 获取标题、描述和第一张图片链接
+    const title = $("title").text();
+    const description = $('meta[name="description"]').attr("content");
+    const firstImgSrc = $("img").first().attr("src");
+    // 返回结果
+    res.status(200).json({ title, description, firstImgSrc, url });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error", error: error.message });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
+export default handler;

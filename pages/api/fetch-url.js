8c75798549deb30cafa1038ec9e1bb9ab3bc3ebf
2chaos <1585767724@qq.com>
2023-06-01 23:03:18 +0800

    用Promise.all() 来同时发起多个请求

--- pages/api/fetch-url.js
+++ pages/api/fetch-url.js
@@ -5,21 +5,18 @@ export default async function handler(req, res) {
   const { url } = req.query;
 
   try {
-    // 发送 GET 请求获取网站 HTML 内容
     const response = await axios.get(url);
 
-    // 使用 cheerio 将 HTML 文本转换成 DOM 对象
     const $ = cheerio.load(response.data);
 
-    // 获取标题、描述和第一张图片链接
-    const title = $("head title").text();
-    const description = $('head meta[name="description"]').attr("content");
-    const firstImgSrc = $("img").first().attr("src");
+    const [title, description, firstImgSrc] = await Promise.all([
+      $("head > title").text(),
+      $('head meta[name="description"]').attr("content"),
+      $("main img").first().attr("src"),
+    ]);
 
-    // 直接返回结果
     res.json({ title, description, firstImgSrc, url });
   } catch (error) {
-    // 处理异常情况，并返回错误信息
     res.status(500).json({ message: "Internal Server Error" });
   }
 }

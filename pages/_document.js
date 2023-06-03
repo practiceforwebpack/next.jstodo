db948caf0c263f935df70ca33c49e66cb5129ac2
2chaos <1585767724@qq.com>
2023-06-03 23:52:30 +0800

    添加了google analytics 跟踪代码

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* 添加Google Analytics代码 */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6CRHNYZW7Q"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-6CRHNYZW7Q');
+            `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

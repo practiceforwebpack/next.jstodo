0b8aa95feb5c0f4c37088fd3e47948670ec4c412
2chaos <1585767724@qq.com>
2023-06-04 14:44:00 +0800

    修改gtag内的参数

--- pages/index.js
+++ pages/index.js
@@ -57,8 +57,9 @@ export default function Home() {
     window.location.href = cardData.url;
 
     // 添加 Google Analytics 代码追踪点击事件
-    gtag("event", "card_click", {
-      event_category: "card",
+    gtag("event", "cardClick", {
+      event_name: "cardClick",
+      event_category: "cardClick",
       event_label: cardData.url,
     });
   };

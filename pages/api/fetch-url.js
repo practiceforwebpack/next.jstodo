08cb8f78a4cf84d7c9e7d7e05898628dd4b44f0c
2chaos <1585767724@qq.com>
2023-06-05 14:15:01 +0800

    添加重定向

--- pages/api/fetch-url.js
+++ pages/api/fetch-url.js
@@ -1,11 +1,19 @@
-import fetch from "node-fetch";
+import fetch, { Request } from "node-fetch";
 import { parse } from "node-html-parser";
 
 export default async function handler(req, res) {
   const { url } = req.query;
+  const requestOptions = {
+    redirect: "follow", // 遵循 follow redirect
+  };
 
   try {
-    const response = await fetch(url);
+    let response = await fetch(new Request(url, requestOptions));
+    let finalUrl = response.url; // 获取最终的 URL
+    if (url !== finalUrl) {
+      console.log(`Redirected to: ${finalUrl}`);
+    }
+
     const html = await response.text();
     const root = parse(html);
     const title = root.querySelector("title").text;
@@ -15,7 +23,7 @@ export default async function handler(req, res) {
     const firstImg = root.querySelector("main img");
     const firstImgSrc = firstImg ? firstImg.getAttribute("src") : null;
 
-    res.json({ title, description, firstImgSrc, url });
+    res.json({ title, description, firstImgSrc, url: finalUrl });
   } catch (e) {
     console.error(`Failed to fetch ${url}: ${e.message}`, e);
     res.status(500).json({ message: "Internal Server Error" });

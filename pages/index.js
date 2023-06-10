b0d999996a2bef4e9bc46beffdd7cd3374bfcf17
2chaos <1585767724@qq.com>
2023-06-10 09:53:19 +0800

    修改card布局 使其左右排列

--- pages/index.js
+++ pages/index.js
@@ -119,60 +119,64 @@ export default function Home() {
           onClick={() => handleClick(cardData.url)}
         >
           <div className={styles.wxcard}>
-            <div
-              className={
-                loading
-                  ? `${styles.wxcardtitle} ${styles.wxcardtitleloading}`
-                  : styles.wxcardtitle
-              }
-            >
-              {loading ? (
-                <>
-                  <Skeleton width={200} height={24} />
-                </>
-              ) : (
-                <h2 className={styles.h2}>{cardData.title}</h2>
-              )}
-            </div>
-            <div className={styles.wxcardcontent}>
-              <div className={styles.wxcarddescription}>
+            <div className={styles.wxcardleft}>
+              <div
+                className={
+                  loading
+                    ? `${styles.wxcardtitle} ${styles.wxcardtitleloading}`
+                    : styles.wxcardleft
+                }
+              >
                 {loading ? (
                   <>
-                    <Skeleton count={7} />
+                    <Skeleton width={200} height={24} />
                   </>
                 ) : (
-                  <p
-                    className={
-                      loading ? styles.p : `${styles.p} ${styles.ploading}`
-                    }
-                  >
-                    {cardData.description}
-                  </p>
+                  <h2 className={styles.h2}>{cardData.title}</h2>
                 )}
               </div>
-              <div className={styles.wxcardimage}>
+              <div className={styles.wxcardcontent}>
+                <div className={styles.wxcarddescription}>
+                  {loading ? (
+                    <>
+                      <Skeleton count={7} />
+                    </>
+                  ) : (
+                    <p
+                      className={
+                        loading ? styles.p : `${styles.p} ${styles.ploading}`
+                      }
+                    >
+                      {cardData.description}
+                    </p>
+                  )}
+                </div>
+              </div>
+              {loading && (
+                <div
+                  className={
+                    loading
+                      ? `${styles.wxcardoverlay} ${styles.wxcardoverlayloading}`
+                      : styles.wxcardoverlay
+                  }
+                >
+                  <div className={styles.wxcardloader} />
+                </div>
+              )}
+            </div>
+            <div className={styles.wxcardright}>
+              <div className={styles.wxcardimg}>
                 {loading ? (
                   <Skeleton width={90} height={90} />
                 ) : (
                   <img
-                    className={styles.img}
+                    className={`${styles.img} ${styles.wxcardright}`}
                     src={cardData.firstImgSrc}
                     alt="图片"
                   />
                 )}
               </div>
             </div>
-            {loading && (
-              <div
-                className={
-                  loading
-                    ? `${styles.wxcardoverlay} ${styles.wxcardoverlayloading}`
-                    : styles.wxcardoverlay
-                }
-              >
-                <div className={styles.wxcardloader} />
-              </div>
-            )}
           </div>
         </a>
 

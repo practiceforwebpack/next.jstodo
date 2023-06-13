811e1c05c23225dbf8e31da2e535ce4fe926ed54
2chaos <1585767724@qq.com>
2023-06-13 23:55:18 +0800

    更新hooks

--- pages/useDataFromLocalStorage.js
+++ pages/useDataFromLocalStorage.js
@@ -1,4 +1,78 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import Skeleton from "react-loading-skeleton";
+
+const Card = ({ cardData, loading }) => {
+  const [imgLoadFailed, setImgLoadFailed] = useState(false);
+
+  useEffect(() => {
+    if (cardData && cardData.title) {
+      document.title = cardData.title;
+    }
+  }, [cardData]);
+
+  const handleImgLoadError = () => {
+    setImgLoadFailed(true);
+  };
+
+  const cardImageUrl =
+    cardData &&
+    cardData.images &&
+    cardData.images.length > 0 &&
+    !imgLoadFailed &&
+    cardData.images[0];
+
+  return (
+    <div className="card">
+      <div className="card-header">
+        {loading ? (
+          <Skeleton height={30} width={200} />
+        ) : (
+          <h2 className="card-title">{cardData.title}</h2>
+        )}
+      </div>
+      <a
+        href={cardData.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="card-body"
+      >
+        {loading ? (
+          <Skeleton height={150} />
+        ) : (
+          <div className="card-image-container">
+            {cardImageUrl ? (
+              <img
+                src={cardImageUrl}
+                alt={cardData.title}
+                className="card-image"
+                onError={handleImgLoadError}
+              />
+            ) : (
+              <div className="card-image-failed">Image not found</div>
+            )}
+          </div>
+        )}
+        <div className="card-content">
+          {!loading && cardData && cardData.description && (
+            <p className="card-description">{cardData.description}</p>
+          )}
+          <span className="card-url">{cardData.url}</span>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  cardData: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    url: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string),
+  }),
+  loading: PropTypes.bool.isRequired,
+};
 
 const useDataFromLocalStorage = (url, urlTitle, yhParams) => {
   const [error, setError] = useState(false);
@@ -18,14 +92,6 @@ const useDataFromLocalStorage = (url, urlTitle, yhParams) => {
 
     if (!isValidURL(url)) {
       setError(true);
-      setLoading(false);
-      return;
-    }
-
-    let data = localStorage.getItem(url);
-
-    if (data) {
-      setCardData(JSON.parse(data));
       setLoading(false);
       return;
     }
@@ -50,10 +116,9 @@ const useDataFromLocalStorage = (url, urlTitle, yhParams) => {
         }
 
         setCardData(data);
-        localStorage.setItem(url, JSON.stringify(data));
       } catch (error) {
         console.error(error);
-        setCardData({ error: "An error occurred" });
+        setError(true);
       }
       setLoading(false);
     };

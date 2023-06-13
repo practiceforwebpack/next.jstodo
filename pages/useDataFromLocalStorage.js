5815cd378226f57c79b3a4b5df5a85bccdc45c91
2chaos <1585767724@qq.com>
2023-06-14 00:17:03 +0800

    更新了hooks

--- pages/useDataFromLocalStorage.js
+++ pages/useDataFromLocalStorage.js
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 
-const useDataFromLocalStorage = (url, title, yhParams) => {
+const useDataFromLocalStorage = (url, urlTitle, yhParams) => {
+  const [error, setError] = useState(false);
   const [cardData, setCardData] = useState({});
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
+    const encodedUrl = encodeURIComponent(url);
+    const encodedTitle = encodeURIComponent(urlTitle);
+    const decodedYhParams = decodeURIComponent(yhParams);
+
+    if (!url) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+
+    if (!isValidURL(url)) {
+      setError(true);
+      setLoading(false);
+      return;
+    }
+
     let data = localStorage.getItem(url);
 
     if (data) {
@@ -18,20 +34,18 @@ const useDataFromLocalStorage = (url, title, yhParams) => {
       setLoading(true);
       try {
         const response = await fetch(
-          `/api/fetch-url?url=${encodeURIComponent(
-            url
-          )}&redirect=false&title=${encodeURIComponent(title)}`
+          `/api/fetch-url?url=${encodedUrl}&redirect=false&title=${encodedTitle}`
         );
         const data = await response.json();
         console.log(data);
         document.title = data.title;
 
-        if (title) {
-          data.title = title;
+        if (urlTitle) {
+          data.title = urlTitle;
         }
 
-        if (yhParams) {
-          const urls = decodeURIComponent(yhParams).split(",");
+        if (decodedYhParams) {
+          const urls = decodedYhParams.split(",");
           data.urls = urls;
         }
 
@@ -39,15 +53,29 @@ const useDataFromLocalStorage = (url, title, yhParams) => {
         localStorage.setItem(url, JSON.stringify(data));
       } catch (error) {
         console.error(error);
-        setError(true);
         setCardData({ error: "An error occurred" });
       }
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [url, urlTitle, yhParams]);
 
-  return { cardData, loading, error };
+  return { error, cardData, loading };
+};
+
+const isValidURL = (url) => {
+  let regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)*\/?$/;
+
+  if (!url.match(regex)) {
+    return false;
+  }
+
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 export default useDataFromLocalStorage;

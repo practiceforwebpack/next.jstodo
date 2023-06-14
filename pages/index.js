ff14964aea1d9aeb260b34c37f37dffc628a1075
2chaos <1585767724@qq.com>
2023-06-14 23:44:13 +0800

    重置

--- pages/index.js
+++ pages/index.js
@@ -4,47 +4,19 @@ import Card from "../components/Card";
 import YhList from "../components/YhList";
 import Icon from "../components/Icon";
 import NotFound from "../components/NotFound";
-import useDataFromLocalStorage from "./useDataFromLocalStorage";
+
 export default function Home() {
   const [error, setError] = useState(false);
+  const [urlParams, setUrlParams] = useState({});
+  const [cardData, setCardData] = useState({});
   const [loading, setLoading] = useState(true);
 
-  const urlParamsTmp = new URLSearchParams(window.location.search);
-  const url = urlParamsTmp.get("url");
-  const urlTitle = urlParamsTmp.get("title");
-  const yhParams = urlParamsTmp.get("yh");
-
-  const encodedUrl = encodeURIComponent(url);
-  const encodedTitle = encodeURIComponent(urlTitle);
-  const decodedYhParams = decodeURIComponent(yhParams);
-
-  const [cardData, cardDataLoading] = useDataFromLocalStorage(url, async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(
-        `/api/fetch-url?url=${encodedUrl}&redirect=false&title=${encodedTitle}`
-      );
-      const data = await response.json();
-      console.log(data);
-      document.title = data.title;
-
-      if (urlTitle) {
-        data.title = urlTitle;
-      }
-
-      if (decodedYhParams) {
-        const urls = decodedYhParams.split(",");
-        data.urls = urls;
-      }
-
-      return data;
-    } catch (error) {
-      console.error(error);
-      return { error: "An error occurred" };
-    }
-  });
-
   useEffect(() => {
+    const urlParamsTmp = new URLSearchParams(window.location.search);
+    const url = urlParamsTmp.get("url");
+    const urlTitle = urlParamsTmp.get("title");
+    const yhParams = urlParamsTmp.get("yh");
+
     if (!url) {
       setError(true);
       setLoading(false);
@@ -56,7 +28,48 @@ export default function Home() {
       setLoading(false);
       return;
     }
-  }, [url]);
+
+    const encodedUrl = encodeURIComponent(url);
+    const encodedTitle = encodeURIComponent(urlTitle);
+    const decodedYhParams = decodeURIComponent(yhParams);
+
+    let data = localStorage.getItem(url);
+
+    if (data) {
+      setCardData(JSON.parse(data));
+      setLoading(false);
+      return;
+    }
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `/api/fetch-url?url=${encodedUrl}&redirect=false&title=${encodedTitle}`
+        );
+        const data = await response.json();
+        console.log(data);
+        document.title = data.title;
+
+        if (urlTitle) {
+          data.title = urlTitle;
+        }
+
+        if (decodedYhParams) {
+          const urls = decodedYhParams.split(",");
+          data.urls = urls;
+        }
+
+        setCardData(data);
+        localStorage.setItem(url, JSON.stringify(data));
+      } catch (error) {
+        console.error(error);
+        setCardData({ error: "An error occurred" });
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
   if (error) {
     return <NotFound />;
@@ -71,7 +84,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Card cardData={cardData} loading={loading || cardDataLoading} />
+        <Card cardData={cardData} loading={loading} />
         {cardData.urls && <YhList urls={cardData.urls} />}
       </main>
     </div>

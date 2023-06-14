c0e3a0523c2e724c55b07222afd9012b2d91c789
2chaos <1585767724@qq.com>
2023-06-15 00:10:31 +0800

    修改文件路径

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,21 @@
 import Head from "next/head";
-import { useState, useEffect } from "react";
 import Card from "../components/Card";
 import YhList from "../components/YhList";
 import Icon from "../components/Icon";
 import NotFound from "../components/NotFound";
+import useDataFromLocalStrong from "../hooks/useDataFromLocalStrong";
 
 export default function Home() {
-  const [error, setError] = useState(false);
-  const [cardData, setCardData] = useState({});
-  const [loading, setLoading] = useState(true);
+  const urlParamsTmp = new URLSearchParams(window.location.search);
+  const url = urlParamsTmp.get("url");
+  const title = urlParamsTmp.get("title");
+  const yhParams = urlParamsTmp.get("yh");
 
-  useEffect(() => {
-    const urlParamsTmp = new URLSearchParams(window.location.search);
-    const url = urlParamsTmp.get("url");
-    const urlTitle = urlParamsTmp.get("title");
-    const yhParams = urlParamsTmp.get("yh");
-
-    if (!url) {
-      setError(true);
-      setLoading(false);
-      return;
-    }
-
-    if (!isValidURL(url)) {
-      setError(true);
-      setLoading(false);
-      return;
-    }
-
-    const encodedUrl = encodeURIComponent(url);
-    const encodedTitle = encodeURIComponent(urlTitle);
-    const decodedYhParams = decodeURIComponent(yhParams);
-
-    let data = localStorage.getItem(url);
-
-    if (data) {
-      setCardData(JSON.parse(data));
-      setLoading(false);
-      return;
-    }
-
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await fetch(
-          `/api/fetch-url?url=${encodedUrl}&redirect=false&title=${encodedTitle}`
-        );
-        const data = await response.json();
-        console.log(data);
-        document.title = data.title;
-
-        if (urlTitle) {
-          data.title = urlTitle;
-        }
-
-        if (decodedYhParams) {
-          const urls = decodedYhParams.split(",");
-          data.urls = urls;
-        }
-
-        setCardData(data);
-        localStorage.setItem(url, JSON.stringify(data));
-      } catch (error) {
-        console.error(error);
-        setCardData({ error: "An error occurred" });
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+  const { cardData, loading, error } = useDataFromLocalStrong(
+    url,
+    title,
+    yhParams
+  );
 
   if (error) {
     return <NotFound />;
@@ -89,18 +36,3 @@ export default function Home() {
     </div>
   );
 }
-
-const isValidURL = (url) => {
-  let regex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w.-]*)*\/?$/;
-
-  if (!url.match(regex)) {
-    return false;
-  }
-
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-};

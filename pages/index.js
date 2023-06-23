2ff5d31faf0f02939f317f9d4d99bc8196e91ba6
2chaos <1585767724@qq.com>
2023-06-23 22:50:14 +0800

    添加form显示编码后的url

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,26 @@ import YhList from "../components/YhList";
 import Icon from "../components/Icon";
 import NotFound from "../components/NotFound";
 import useDataFromLocalStrong from "../hooks/useDataFromLocalStrong";
+import { useState, useEffect } from "react";
+import { Form, Input } from "antd";
 
 export default function Home() {
   const urlParamsTmp =
     typeof window !== "undefined" &&
     new URLSearchParams(window.location.search);
   const url = urlParamsTmp && urlParamsTmp.get("url");
+  const encodedUrl = encodeURIComponent(url || "");
   const title = urlParamsTmp && urlParamsTmp.get("title");
   const yhParams = urlParamsTmp && urlParamsTmp.get("yh");
+
+  const [form] = Form.useForm();
+  const [initialValues, setInitialValues] = useState({ url: encodedUrl });
+
+  useEffect(() => {
+    if (encodedUrl) {
+      setInitialValues({ url: encodedUrl });
+    }
+  }, [url]);
 
   const { cardData, loading, error } = useDataFromLocalStrong(
     url,
@@ -34,6 +46,11 @@ export default function Home() {
       <main>
         <Card cardData={cardData} loading={loading} />
         {cardData.urls && <YhList urls={cardData.urls} />}
+        <Form layout="vertical" form={form} initialValues={initialValues}>
+          <Form.Item label="Encoded URL" name="url">
+            <Input.TextArea rows={1} readOnly />
+          </Form.Item>
+        </Form>
       </main>
     </div>
   );

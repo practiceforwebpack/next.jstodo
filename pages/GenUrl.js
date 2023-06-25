8b55baf7998cb528718fb9f40cfea20fbeb1f669
2chaos <1585767724@qq.com>
2023-06-25 22:36:15 +0800

    增添了yh url 的同步编码

--- pages/GenUrl.js
+++ pages/GenUrl.js
@@ -6,11 +6,28 @@ const GenUrl = () => {
   const router = useRouter();
   const [url, setUrl] = useState("");
   const [encodedUrl, setEncodedUrl] = useState("");
+  const [additionalUrls, setAdditionalUrls] = useState([]);
+
+  const handleAdditionalUrlChange = (index, value) => {
+    const newUrls = [...additionalUrls];
+    newUrls[index] = value;
+    setAdditionalUrls(newUrls);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const encodeUrl = `http://localhost:3000/?url=${encodeURIComponent(url)}`;
-    setEncodedUrl(encodeUrl);
+    const encodedAdditionalUrls = additionalUrls
+      .map((additionalUrl) => encodeURIComponent(additionalUrl))
+      .join(",");
+    const encodedUrl = `http://localhost:3000/?url=${encodeURIComponent(
+      url
+    )}&yh=${encodedAdditionalUrls}`;
+    setEncodedUrl(encodedUrl);
+  };
+
+  const handleAddUrl = (e) => {
+    e.preventDefault();
+    setAdditionalUrls([...additionalUrls, ""]);
   };
 
   return (
@@ -21,9 +38,17 @@ const GenUrl = () => {
           onChange={(e) => setUrl(e.target.value)}
           value={url}
         />
+        {additionalUrls.map((additionalUrl, index) => (
+          <Input
+            key={index}
+            placeholder="请输入YH-URL"
+            value={additionalUrl}
+            onChange={(e) => handleAdditionalUrlChange(index, e.target.value)}
+          />
+        ))}
+        <Button onClick={handleAddUrl}>添加URL</Button>
         <Button htmlType="submit">提交</Button>
       </form>
-
       {encodedUrl && (
         <Form style={{ marginTop: "20px" }} initialValues={{ encodedUrl }}>
           <Form.Item label="编码后URL" name="encodedUrl">

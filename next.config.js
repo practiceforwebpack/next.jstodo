00b1b3b0831946a4a515f55b643d06d4519395d9
2chaos <1585767724@qq.com>
2023-07-05 00:10:23 +0800

    重置了next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "/api/:path*",
-      },
-    ];
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
 };
+
+module.exports = nextConfig;
